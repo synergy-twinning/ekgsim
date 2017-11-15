ae1074d418e62c0c3c5932b1c7b950a87113008b
Matjaž Depolli <matjaz.depolli@ijs.si>
2017-11-15 09:28:25 +0100

    fixed hyper.sh script

--- hyper.sh
+++ hyper.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# takes an optional argument - the name of input file
+inFile=${1-"testRun/individuals.txt"}
+
 # takes individuals file as an input and outputs hypervolume on std.out
-echo ${1-"individuals.txt"} 
-../AMS-DEMO/DEMO -individuals:${1-"individuals.txt"} -analysis:hypervolume -gen:0:-1 > hyper.txt
-cat hyper.txt
+echo -e "using input file:\n   $inFile"
+echo -e "output will be written to file:\n   hyper.txt"
+
+./AMS-DEMO/DEMO -individuals:${inFile} -analysis:hypervolume -params:0,2:0,2 -gen:0:-1 > hyper.txt
+# optionally, you might wish to let AMS-DEMO calculate the limits for hypervolume by itself; this can be done by using (uncomment it) the line below:
+#./AMS-DEMO/DEMO -individuals:${inFile} -analysis:hypervolume -gen:0:-1 > hyper.txt
+
+echo ""
